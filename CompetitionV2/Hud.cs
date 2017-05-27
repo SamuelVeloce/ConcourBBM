1ae4e97477128c0990557e336fb0c324103aa6e7
Frédérick Picard <frederick.picard@hotmail.ca>
2017-05-27 14:04:48 -0400

    Ajustement du son mondif des infos du hud, reduction du son du pistolet et musique in game ajoutee

--- CompetitionV2/Hud.cs
+++ CompetitionV2/Hud.cs
@@ -24,10 +24,31 @@ namespace TopDownGridBasedEngine
 
         public void Draw(SpriteBatch sb)
         {
+            //Gestion du statut de l'armes (Balles)
+            int nbBalles = EntityManager.Instance.Joueur.CurrentWeapon().NBulletInCharger;
+            int nbTotBalles = EntityManager.Instance.Joueur.CurrentWeapon().NBulletLeft;
+            string strBalles = "Balles: ";
+            if (nbBalles == 0)
+            {
+                if (nbTotBalles == 0)
+                {
+                    strBalles += "Vide";
+                }
+                else
+                {
+                    strBalles += "Recharg.";
+                }
+            }
+            else
+            {
+                strBalles += nbBalles.ToString();
+            }
+
+            
             //Dessine les informations dans le coin supérieur droit
             sb.DrawString(TextureManager.Font, EntityManager.Instance.Joueur.CurrentWeapon().Nom, new Vector2(Game1.Screen.ClientBounds.Width - 150, 10), Color.Yellow);
             sb.DrawString(TextureManager.Font, EntityManager.Instance.Entities.Count.ToString() + " Enemis restant", new Vector2(Game1.Screen.ClientBounds.Width - 150, 30), Color.Yellow);
-            sb.DrawString(TextureManager.Font,"Balles: " + EntityManager.Instance.Joueur.CurrentWeapon().NBulletInCharger.ToString() , new Vector2(Game1.Screen.ClientBounds.Width - 150, 50),Color.Yellow);
+            sb.DrawString(TextureManager.Font,strBalles , new Vector2(Game1.Screen.ClientBounds.Width - 150, 50),Color.Yellow);
         }
     }
 }
