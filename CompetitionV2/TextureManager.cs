5ad2e7bbe50d817c9af241b5d359efad04ca1627
DorionQc <gouletsamuel@hotmail.com>
2017-05-28 10:27:17 -0400

    STABLE

--- CompetitionV2/TextureManager.cs
+++ CompetitionV2/TextureManager.cs
@@ -143,8 +143,8 @@ namespace TopDownGridBasedEngine
             TexturePlayerRight[3] = TexturePlayerRight[1];
             TexturePlayerLeft[3] = _content.Load<Texture2D>("Textures/BrownGuyLeft3");
             //TextureBomb[2] = TextureBomb[0];
-            TexturePlayerDown[2] = TexturePlayerUp[0];
-            TexturePlayerUp[2] = TexturePlayerDown[0];
+            TexturePlayerDown[2] = TexturePlayerDown[0];
+            TexturePlayerUp[2] = TexturePlayerUp[0];
             TexturePlayerRight[2] = TexturePlayerRight[0];
             TexturePlayerLeft[2] = TexturePlayerLeft[0];
 
